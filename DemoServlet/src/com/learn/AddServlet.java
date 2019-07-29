ae939d8f25d8379cdad07c76f8f78eaf332f393f
pratheeban <40929176+Pratheeban233@users.noreply.github.com>
2019-07-29 22:38:09 +0530

    1) HttpSession and cookie 2) Servlet context and Config

--- DemoServlet/src/com/learn/AddServlet.java
+++ DemoServlet/src/com/learn/AddServlet.java
@@ -4,39 +4,75 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.sun.corba.se.spi.servicecontext.ServiceContext;
 
 public class AddServlet extends HttpServlet
 {
 	public void Post(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException
 	{
-		int i=Integer.parseInt(req.getParameter("t1"));
-		int j=Integer.parseInt(req.getParameter("t2"));
 		
-		int k=i+j;
 		
+		  int i=Integer.parseInt(req.getParameter("t1")); int
+		  j=Integer.parseInt(req.getParameter("t2"));
+		  
+		  int k=i+j;
+		 
+
 		/*
+		 * //RequestDispatcher
+		 * 
 		 * req.setAttribute("k", k);
+		 */
+		  
+		  //ServeltContextConfig//
+		/*
+		 * PrintWriter out = res.getWriter(); out.println("Inside AddServlet ");
 		 * 
-		 * PrintWriter out=res.getWriter(); out.println("Inside AddServlet");
+		 * out.println("Inside servletContextConfig ");
 		 * 
+		 * ServletContext ctx = getServletContext(); String str =
+		 * ctx.getInitParameter("name3"); out.print(str);
+		 * 
+		 * ServletConfig cnf = getServletConfig(); String str1 =
+		 * cnf.getInitParameter("name1"); out.print(str1);
+		 * 
+		 * out.println("after servelt context");
+		 */
+
+		/*
 		 * RequestDispatcher rd=req.getRequestDispatcher("sq"); rd.forward(req, res);
 		 */
+
+		// Session
+		/*
+		 * HttpSession session=req.getSession(); session.setAttribute("k", k);
+		 */
+
 		
-		res.sendRedirect("sq?k="+k);  // send redirect to server 
-		
+		  Cookie cookie=new Cookie("k", k+""); res.addCookie(cookie);
+		 
+
+		// res.sendRedirect("sq?k="+k); // send redirect to server by url
+		 res.sendRedirect("sq");
+
 	}
-	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException
-	{
-		AddServlet as=new AddServlet();
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		AddServlet as = new AddServlet();
 		as.Post(req, res);
 	}
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
-	{
-		AddServlet as= new AddServlet();
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		AddServlet as = new AddServlet();
 		as.Post(req, res);
 	}
 }
--- DemoServlet/src/com/learn/SqServlet.java
+++ DemoServlet/src/com/learn/SqServlet.java
@@ -3,21 +3,52 @@ package com.learn;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SqServlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
-	{
-		//int k=(int) req.getAttribute("k");
-		int k=Integer.parseInt(req.getParameter("k"));
-		k=k*k;
+	{	
+		int k = 0;
+		/*
+		 * //session HttpSession session=req.getSession(); int k=(int)
+		 * session.getAttribute("k");
+		 */
+
+		Cookie[] cookies = req.getCookies();
+
+		for (Cookie c : cookies) {
+			if (c.getName().equals("k"))
+				k = Integer.parseInt(c.getValue());
+		}
+
+		// int k=(int) req.getAttribute("k");
+		// int k=Integer.parseInt(req.getParameter("k"));
+		k = k * k;
+
+		PrintWriter out = res.getWriter();
+		out.println("Inside SqServlet<br>");
+
+		out.println("result is:" + k);
 		
-		PrintWriter out=res.getWriter();
-		out.println("Inside SqServlet");
-	
-		out.println("result is:"+k);
+		out.println("<br>");
+		//PrintWriter out = res.getWriter();
+		out.println("Inside servletContextConfig :: ");
+		
+		ServletConfig cnf = getServletConfig();
+		String str1 = cnf.getInitParameter("name2");
+		out.println(str1);
+		
+		ServletContext ctx = getServletContext(); 
+		String str = ctx.getInitParameter("name3"); 
+		out.println(str);
+		 
+		out.println("<br>after serveltcontextconfig");
 	}
 }
